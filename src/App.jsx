a1e777d31e354a301616ca320d24d5a27db79792
Web Dev Atanu <atanushil358@gmail.com>
2024-08-11 20:12:49 +0530

    Starter for Vite+react

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen w-screen flex-col">
+      <h1 className="underline">This is the starter for Vite + React.</h1>
+    </div>
+  );
+}
+
+export default App;
