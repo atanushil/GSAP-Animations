63f7750953f5606ca8d8686c002136af05099a2b
Web Dev Atanu <atanushil358@gmail.com>
2024-08-11 20:37:20 +0530

    Starter for VITE+REACT+TAILWIND+GSAP

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen w-screen">
+      <p className="text-black text-[2rem] uppercase">
+        This is the starter for 
+        <a href="https://tailwindcss.com/docs/guides/vite" target="_blank" className="hover:text-blue-400 px-2"> Vite + React + tailwind + </a>
+        <a href="https://gsap.com/docs/v3/GSAP/" target="_blank" className="hover:text-green-400">GSAP</a>
+      </p>
+    </div>
+  );
+}
+
+export default App;
