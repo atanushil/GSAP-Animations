e4cc02f6f584be9c984346d90f8d85553360a711
Web Dev Atanu <atanushil358@gmail.com>
2024-08-11 20:16:24 +0530

    Revert "Starter for Vite+react"
    This reverts commit a1e777d31e354a301616ca320d24d5a27db79792.
    
    ear

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="flex justify-center items-center h-screen w-screen flex-col">
-      <h1 className="underline">This is the starter for Vite + React.</h1>
-    </div>
-  );
-}
-
-export default App;
